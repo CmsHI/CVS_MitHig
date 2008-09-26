d9cbcf8b63cb7d3a4aba075e4176666141e28269
yilmaz <>
2008-09-26 09:31:29 +0000

    Temporary fix for fake conditions

--- Configuration/python/FakeConditions_TempFix_cff.py
+++ Configuration/python/FakeConditions_TempFix_cff.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FakeConditions_cff import *
 
 siPixelCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/siPixelCabling200.db'
-cscPackingCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/CSCChamberMapValues.db'
-cscUnpackingCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/CSCCrateMapValues.db'
+cscPackingCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/CSCChamberMapValues_18X.db'
+cscUnpackingCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/CSCCrateMapValues_18X.db'
 DTCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/DTFullMap_fix17X.db'
 RPCCabling.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/RPCEMap_181007.db'
 trackProbabilityFakeCond.connect = 'sqlite_file:/net/pstore01/d00/scratch/yetkin/cms/CondCore/SQLiteData/data/btagTrackProbability200.db'
