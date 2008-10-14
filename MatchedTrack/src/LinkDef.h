a2fe939f78e0a77882355425fb4bec6567b07980
edwenger <>
2008-10-14 15:55:06 +0000

    LinkDef for MatchedTrack class

---
+++ MatchedTrack/src/LinkDef.h
@@ -0,0 +1,15 @@
+#ifndef ROOT_MATCHEDTRACK_H
+#include "edwenger/MatchedTrack/interface/MatchedTrack.h"
+#define ROOT_MATCHEDTRACK_H
+#endif
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+#pragma link C++ class MatchedTrack+;
+
+#endif
