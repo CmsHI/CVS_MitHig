aeec7c80c0abafde3ff481862de6076480f6e043
edwenger <>
2008-10-24 19:19:42 +0000

    updated to use TrackerLocalReco_SimData and HighPtTracking_PbPb

--- HighPtTrackAnalyzer/prod/TrackingWithHighPtAnalyzer_cfg.py
+++ HighPtTrackAnalyzer/prod/TrackingWithHighPtAnalyzer_cfg.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("RECO")
 
 process.load("Configuration.StandardSequences.Services_cff")
-process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
 process.load("Configuration.StandardSequences.Simulation_cff")
 process.load("Configuration.StandardSequences.FakeConditions_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
@@ -13,11 +12,13 @@ process.load("Configuration.StandardSequences.Digi_cff")
 process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
 process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
 
-process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
+process.load("RecoLocalTracker.Configuration.RecoLocalTracker_SimData_cff")
+#process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
 
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 process.load("RecoVertex.BeamSpotProducer.BeamSpotFakeConditionsEarlyCollision_cff")
-process.load("HeavyIonsAnalysis.Configuration.HeavyIonTracking_cff")
+#process.load("HeavyIonsAnalysis.Configuration.HeavyIonTracking_cff")
+process.load("HeavyIonsAnalysis.Configuration.HighPtTracking_PbPb_cff")
 
 ###############################################################################
 # Message logger
@@ -33,15 +34,28 @@ process.MessageLogger = cms.Service("MessageLogger",
 process.source = cms.Source("PoolSource",
     skipEvents = cms.untracked.uint32(0),
     fileNames  = cms.untracked.vstring(
-        #'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/pythia_mb_5TeV_d20080823/pythia_mb_5TeV_d20080823_r000001.root'
-		'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_sim_x2_mb_d20080819/hydjet_sim_x2_mb_d20080819_r000211.root'
+        #'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/pythia_dijet_pt100to9999_d20081021/pythia_dijet_pt100to9999_d20081021_r000001.root'
+		#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/pythia_mb_5TeV_d20080823/pythia_mb_5TeV_d20080823_r000002.root'
+		#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_sim_x2_mb_d20080819/hydjet_sim_x2_mb_d20080819_r000211.root'
+		#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_sim_x2_c1_d20080819/hydjet_sim_x2_c1_d20080819_r000002.root'
+		'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_x2_b0_d20081020/hydjet_x2_b0_d20081020_r000116.root'
 		)
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(1)
 )
-			
+		
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")			
+					
+#from HeavyIonsAnalysis.Configuration.EventEmbedding_cff import *
+#process.mix=mixSim
+#process.mix.input.fileNames = cms.untracked.vstring(
+			#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_sim_x2_mb_d20080819/hydjet_sim_x2_mb_d20080819_r000211.root'
+			#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/pythia_dijet_pt100to9999_d20080731/pythia_dijet_pt100to9999_d20080731_r000037.root'
+			#'dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/yetkin/sim/hydjet_x2_b0_d20081020/hydjet_x2_b0_d20081020_r000116.root'
+#)			
+				
 ###############################################################################
 # Random Number Seeds
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
