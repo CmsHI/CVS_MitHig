59847d74bbffa4c363ed0a321036a10c11aa7506
edwenger <>
2009-04-22 08:35:33 +0000

    appended Sim to path names to clarify that this runs on simDigis

--- Configuration/python/RecoLocalTracker_SimData_cff.py
+++ Configuration/python/RecoLocalTracker_SimData_cff.py
@@ -12,8 +12,8 @@ from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_SimData2_cfi import 
 from RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi import *
 siPixelClusters.src = 'simSiPixelDigis' #sim
 from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
-pixeltrackerlocalreco = cms.Sequence(siPixelClusters*siPixelRecHits)
-striptrackerlocalreco = cms.Sequence(siStripZeroSuppression*siStripClusters*siStripMatchedRecHits)
-trackerlocalreco = cms.Sequence(pixeltrackerlocalreco*striptrackerlocalreco)
+pixeltrackerlocalrecoSim = cms.Sequence(siPixelClusters*siPixelRecHits)
+striptrackerlocalrecoSim = cms.Sequence(siStripZeroSuppression*siStripClusters*siStripMatchedRecHits)
+trackerlocalrecoSim = cms.Sequence(pixeltrackerlocalrecoSim*striptrackerlocalrecoSim)
 
 
