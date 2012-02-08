16dcb04080453654e2b18ce187d9b4e22d4cf1a2
davidlw <>
2012-02-08 01:43:12 +0000

    Updated

--- PixelTrackletAnalyzer/test/trackanalyzer_cfg.py
+++ PixelTrackletAnalyzer/test/trackanalyzer_cfg.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.GeometryExtended_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -11,6 +13,9 @@ process.source = cms.Source("PoolSource",
         'file:/mnt/hadoop/cms/store/user/davidlw/Hydjet_Bass_MinBias_2760GeV/Summer11-STARTHI44_V4-v1-GEN-SIM-RECO_v1/a3a91e311bdf37073736c6b7978adab1/hiReco_RAW2DIGI_RECO_1938_3_znQ.root'
     )
 )
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'STARTHI44_V7::All'
 
 process.demo = cms.EDAnalyzer('TrackAnalyzer',
                           trackPtMin = cms.untracked.double(0.4),
